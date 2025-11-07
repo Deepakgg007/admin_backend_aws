2c2af8d16efe80bbc9cf92de685a4d8d24d83c9f
Deepakgg007 <deepakg08199@gmail.com>
2025-11-07 16:40:25 +0530

    logo issue reolved

--- z1_backend/urls.py
+++ z1_backend/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media and static files in development
+# Note: In production, use a proper web server (nginx/apache) to serve these files
 if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
