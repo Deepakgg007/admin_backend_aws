340626b0949fa63f113062f769139eeeef69d7dd
Ubuntu <ubuntu@ip-172-31-37-255.eu-north-1.compute.internal>
2026-02-11 10:38:15 +0000

    final changes for ai

--- code/script.py
+++ code/script.py
@@ -1,38 +1,7 @@
-def spiralPrint(matrix):
-    res = []
-    m, n = len(matrix), len(matrix[0])
-    
-    top, bottom = 0, m - 1
-    left, right = 0, n - 1
-
-    while top <= bottom and left <= right:
-
-        # left → right
-        for i in range(left, right + 1):
-            res.append(matrix[top][i])
-        top += 1
-
-        # top → bottom
-        for i in range(top, bottom + 1):
-            res.append(matrix[i][right])
-        right -= 1
-
-        if top <= bottom:
-            # right → left
-            for i in range(right, left - 1, -1):
-                res.append(matrix[bottom][i])
-            bottom -= 1
-
-        if left <= right:
-            # bottom → top
-            for i in range(bottom, top - 1, -1):
-                res.append(matrix[i][left])
-            left += 1
-
-    return res
-
-
-if __name__=="__main__":
-    m,n=map(int,input().split())
-    matrix=[list(map(int,input().split())) for _ in range(m)]
-    print(*spiralPrint(matrix))
+def kth_smallest(times, k):
+    times.sort()
+    return times[k-1]
+n = int(input().strip())
+times = list(map(int, input().split()))
+k = int(input().strip())
+print(kth_smallest(times, k))
